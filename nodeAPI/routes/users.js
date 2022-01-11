32d6de226faaaa8d5c4b1a522214b43a0cb0bb88
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-11 12:15:28 +0200

    POST routes

--- nodeAPI/routes/users.js
+++ nodeAPI/routes/users.js
@@ -1,10 +1,28 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
+const users = [];
+
 //all routes here start with /users
 router.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+  //users with ids
+  users.push({ ...user, id: uuidv4() });
+
+  res.send(`user with name ${user.firstname} added to the Database`);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id == id);
+
+  res.send(foundUser);
 });
 
 export default router;
