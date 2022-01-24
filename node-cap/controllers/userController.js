53cb9ca6da801fcb69c4b5ae8234effea3051d94
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2022-01-24 22:35:24 +0200

    user routes refactoring

--- node-cap/controllers/userController.js
+++ node-cap/controllers/userController.js
@@ -1,26 +1,95 @@
-export const getLogout = (req, res, next) => {
-    if (req.cookie) {
-        req.session.destroy();
-        res.clearCookie('session_id');
-        res.redirect('/');
-    }
-    else {
-        var err = new Error('You are not logged in!');
-        err.status = 403;
-        next(err);
-    }
+import bcrypt from 'bcrypt';
+import jwt from "jsonwebtoken";
+import userModel from "../models/user.js";
+
+const Users = userModel;
+
+export const userLogout = (req, res) => {
+    return res.status(200).clearCookie('jwt').json({ message: "Successfully logged out 😏 🍀" });
 }
 
 export const getUser = (req, res, next) => {
+    Users.find({}).then((users) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(users);
+    }, (err) => next(err))
+        .catch((err) => next(err));
+}
+
+export const userSignup = async (req, res, next) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10); //salt gen & hash the password.
+
+        const user = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: hashedPassword
+        }; //store the hashed password
+
+
+        Users.create(user).then((user) => {
+            console.log(` ${user.firstname}'s account has been created successfully!`);
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(user)
+        }, (err) => next(err))
+            .catch((err) => next(err));
+
+
+    } catch (error) {
+        res.statusCode = 500;
+        res.send('Looks like it\'s our problem, we\'ll solve it in no time');
+
+    }
 
 }
-/* export const findUser = (req, res, next) => {
-    const user = await Users.find({ 'email': `${req.body.email}` });
+export const findUser = (req, res, next) => {
     Users.findOne({ 'email': `${req.body.email}` }, 'email password', function (err, user) {
         if (err) return handleError(err);
-        // Prints "Space Ghost is a talk show host".
+        res.status(401);
+        console.log(' The user with this email %s exist, try creating your email', user.email);
+    });/* 
+    next() */
+}
+
+export const userLogin = async (req, res, next) => {
+    //authenticate user
+
+    Users.findOne({ 'email': `${req.body.email}` }, 'email password', function (err, user) {
+        if (user == null) {
+            return res.status(400).send("Cannot find user with that Email")
+        }
+
         console.log('%s is a %s.', user.email,
             user.password);
-    });
-    next()
-} */
+        //comparing the saved password with provided one
+
+        try {
+            if (bcrypt.compare(req.body.password, user.password)) {
+                const userMail = { email: req.body.email };
+                //sign the email
+                const accessToken = jwt.sign(userMail, config.ACCESS_TOKEN_SECRET);
+
+                res.status(200).cookie('jwt', accessToken, {
+                    maxAge: 1000 * 60 * 15, //after 15mini
+                    sameSite: 'strict',
+                    httpOnly: true
+                });
+
+                res.setHeader('Content-Type', 'application/json');
+                res.json({ success: true, token: accessToken, status: 'You are successfully logged in!' });
+            }
+            else {
+                res.send('Not allowed');
+            }
+        }
+        catch (error) {
+            res.statusCode = 500;
+            res.send('Looks like it\'s our problem, we\'ll solve it in no time');
+        }
+    })
+
+}
--- node-cap/routes/user.js
+++ node-cap/routes/user.js
@@ -1,18 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
+
 import bcrypt from 'bcrypt';
 import cookieParser from "cookie-parser";
 
 import config from "../config.js";
 import { authorization } from "../Middleware/authentication.js";
-import { getLogout } from "../controllers/userController.js";
+import { getUser, findUser, userSignup, userLogin, userLogout } from "../controllers/userController.js";
 import userModel from "../models/user.js"; //user model
 
 const userRouter = express.Router();
 userRouter.use(express.json());
 userRouter.use(cookieParser());
-const Users = userModel;
 
 
 userRouter.route('/')
@@ -21,94 +19,16 @@ userRouter.route('/')
         res.setHeader('Content-Type', 'text/plain');
         next()
     })
-    .get((req, res, next) => {
-        Users.find({}).then((users) => {
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.json(users);
-        }, (err) => next(err))
-            .catch((err) => next(err));
-    });
+    .get(getUser);
 
 /* ========================SIGNUP======================= */
 userRouter
-    .post('/signup', async (req, res, next) => {
-
-        try {
-            const hashedPassword = await bcrypt.hash(req.body.password, 10); //salt gen & hash the password.
-
-            const user = {
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                email: req.body.email,
-                password: hashedPassword
-            }; //store the hashed password
-
-
-            Users.create(user).then((user) => {
-                console.log(` ${user.firstname}'s account has been created successfully!`);
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
-                res.json(user)
-            }, (err) => next(err))
-                .catch((err) => next(err));
-
-
-        } catch (error) {
-            res.statusCode = 500;
-            res.send('Looks like it\'s our problem, we\'ll solve it in no time');
-
-        }
-
-    });
+    .post('/signup', findUser, userSignup);
 
 /* ===================LOGIN============================ */
-userRouter.post('/login', async (req, res, next) => {
-    //authenticate user
-    //const user = await Users.find({ 'email': `${req.body.email}` });
-
-    Users.findOne({ 'email': `${req.body.email}` }, 'email password', function (err, user) {
-        if (user == null) {
-            return res.status(400).send("Cannot find user with that Email")
-        }
-
-        console.log('%s is a %s.', user.email,
-            user.password);
-        //comparing the saved password with provided one
-
-        try {
-            if (bcrypt.compare(req.body.password, user.password)) {
-                const userMail = { email: req.body.email };
-                //sign the email
-                const accessToken = jwt.sign(userMail, config.ACCESS_TOKEN_SECRET);
-
-                res.status(200).cookie('jwt', accessToken, {
-                    maxAge: 1000 * 60 * 15, //after 15mini
-                    sameSite: 'strict',
-                    httpOnly: true
-                });
-
-                res.setHeader('Content-Type', 'application/json');
-                res.json({ success: true, token: accessToken, status: 'You are successfully logged in!' });
-            }
-            else {
-                res.send('Not allowed');
-            }
-        }
-        catch (error) {
-            res.statusCode = 500;
-            res.send('Looks like it\'s our problem, we\'ll solve it in no time');
-        }
-    })
-
-});
-
-
+userRouter.post('/login', userLogin);
 
 /* =====================LOGOUT======================= */
-userRouter.get('/logout', authorization, (req, res,) => {
-    return res.status(200).clearCookie('jwt').json({ message: "Successfully logged out 😏 🍀" });
-}); //getLogout controller
-
+userRouter.get('/logout', authorization, userLogout);
 
 export default userRouter;
